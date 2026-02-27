c2d6626ed6eb29620cd3ff354dac4d165ef889d4
Anaghapramodc <anaghapramod916@gmail.com>
2026-02-28 03:25:33 +0530

    Remove staticfiles; let Django collect it at deploy

--- staticfiles/js/serviceworker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('push', function(e) {
-    const data = e.data.json();
-
-    self.registration.showNotification(data.head, {
-        body: data.body,
-        icon: '/static/images/logo.png'
-    });
-});
