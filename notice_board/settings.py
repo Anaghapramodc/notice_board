819a266e520d2f74f69b86eb1dc3aa4b5245ca72
Anaghapramodc <anaghapramod916@gmail.com>
2026-02-28 15:39:52 +0530

    https://github.com/Anaghapramodc/notice_board.git

--- notice_board/settings.py
+++ notice_board/settings.py
@@ -185,15 +185,3 @@ WEBPUSH_SETTINGS = {
 }
 
 
-
-import os
-
-if os.environ.get("RAILWAY_ENVIRONMENT"):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@gmail.com",
-            password="admin123"
-        )
